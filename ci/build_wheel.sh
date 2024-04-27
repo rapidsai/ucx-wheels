5aada8013238599c36b4253f26909986f621678a
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-27 01:25:41 +0000

    Exclude libcuda.so, see what happens with libnvidia-ml.so

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -15,5 +15,9 @@ PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
 sed -i -E "s/^name = \"${package_name}(.*)?\"$/name = \"${package_name}${PACKAGE_CUDA_SUFFIX}\"/g" ${pyproject_file}
 
 python -m pip wheel "${package_dir}"/ -w "${package_dir}"/dist -vvv --no-deps --disable-pip-version-check
-python -m auditwheel repair -w ${package_dir}/final_dist ${package_dir}/dist/*
+
+# We must avoid bundling libcuda.so. What about libnvidia-ml.so? We had long
+# discussions about this in the past, and at the time we settled on excluding
+# it since we were going to be non-compliant without libcuda.so anyway.
+python -m auditwheel repair -w ${package_dir}/final_dist --exclude "libcuda.so" ${package_dir}/dist/*
 RAPIDS_PY_WHEEL_NAME="ucx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp ${package_dir}/final_dist
