e06ee695ea5bde624ca3fe79015ba45a6d752216
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-27 00:57:27 +0000

    Fix load

--- python/libucx/libucx/load.py
+++ python/libucx/libucx/load.py
@@ -17,16 +17,29 @@ import ctypes
 import os
 
 
+# IMPORTANT: The load order here matters! libucm.so depends on symbols in the other
+# libraries being loaded first, but does not express this via a DT_NEEDED entry.
+UCX_LIBRARIES = [
+    "libucp.so",
+    "libuct.so",
+    "libucs.so",
+    "libucs_signal.so",
+    "libucm.so",
+]
+
 def load_library():
     # Dynamically load libucx.so. Prefer a system library if one is present to
     # avoid clobbering symbols that other packages might expect, but if no
     # other library is present use the one in the wheel.
-    try:
-        libucx_lib = ctypes.CDLL("libucx.so", ctypes.RTLD_GLOBAL)
-    except OSError:
-        libucx_lib = ctypes.CDLL(
-            os.path.join(os.path.dirname(__file__), "lib", "libucx.so"),
-            ctypes.RTLD_GLOBAL,
-        )
+    libraries = []
+    for lib in UCX_LIBRARIES:
+        try:
+            libucx_lib = ctypes.CDLL(lib, ctypes.RTLD_GLOBAL)
+        except OSError:
+            libucx_lib = ctypes.CDLL(
+                os.path.join(os.path.dirname(__file__), "lib", lib),
+                ctypes.RTLD_GLOBAL,
+            )
+        libraries.append(libucx_lib)
 
-    return libucx_lib
+    return libraries
