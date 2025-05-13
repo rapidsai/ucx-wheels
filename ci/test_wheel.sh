5815a01b379b4b149facec4235bdf8c706b3f06b
James Lamb <jaylamb20@gmail.com>
2025-05-13 16:51:50 -0500

    Use GitHub Actions artifact store (#23)
    Contributes to https://github.com/rapidsai/build-infra/issues/237
    
    Switches from `downloads.rapids.ai` to the GitHub Actions artifact store
    for passing wheels from build jobs to testing and publishing jobs.
    
    This is part of an ongoing effort across RAPIDS to remove reliance on
    `downloads.rapids.ai`.
    
    ## Notes for Reviewers
    
    ### How I tested this
    
    Saw the artifacts on the "artifacts" view of the run summary, and saw
    them used in the test jobs.
    
    ```text
    [rapids-download-from-github] Downloading and decompressing ucx-wheels_wheel_cpp_ucx_cu11_aarch64 from Run ID 15002354913 into /tmp/tmp.hdYWbJyHYf
    ...
    Processing /tmp/tmp.hdYWbJyHYf/libucx_cu11-1.18.1a0-py3-none-manylinux_2_28_aarch64.whl
    Installing collected packages: libucx-cu11
    Successfully installed libucx-cu11-1.18.1a0
    ```
    
    ([build
    link](https://github.com/rapidsai/ucx-wheels/actions/runs/15002354913/job/42152989648?pr=23))

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -8,9 +8,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 package_name="libucx"
 
-WHEELHOUSE="${PWD}/dist/"
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="ucx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp "${WHEELHOUSE}"
+WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="ucx_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 python -m pip install "${WHEELHOUSE}/${package_name}_${RAPIDS_PY_CUDA_SUFFIX}"*.whl
 
 # Test basic library loading
