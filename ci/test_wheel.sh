8f9e29c2dd76fefc8dab5bd5ed04c47436cf11fe
Bradley Dice <bdice@bradleydice.com>
2026-07-24 16:46:51 -0500

    test: assert native library loads

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 set -euo pipefail
 
@@ -20,13 +20,13 @@ rapids-pip-retry install \
   --prefer-binary \
   --constraint "${PIP_CONSTRAINT}" \
   "${WHEELHOUSE}/${package_name}_${RAPIDS_PY_CUDA_SUFFIX}"*.whl
-python -c "import libucx; libucx.load_library()"
+python -c "import libucx; assert all(libucx.load_library())"
 deactivate
 
 python -m pip install "${WHEELHOUSE}/${package_name}_${RAPIDS_PY_CUDA_SUFFIX}"*.whl
 
 # Test basic library loading
-python -c "import libucx; libucx.load_library(); print('Loaded libucx libraries successfully!')"
+python -c "import libucx; assert all(libucx.load_library()); print('Loaded libucx libraries successfully!')"
 
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 REFERENCE_FILE="${SCRIPT_DIR}/symbols_cuda${RAPIDS_CUDA_MAJOR}.txt"
